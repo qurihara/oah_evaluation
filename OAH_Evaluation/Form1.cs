a2afd86c8a38858567d7d281d7998ad1038356ef
qurihara <qurihara@gmail.com>
2015-07-15 09:28:43 +0900

    ok

--- OAH_Evaluation/Form1.cs
+++ OAH_Evaluation/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sharpduino;
+using Sharpduino.Constants;
 
 namespace OAH_Evaluation
 {
@@ -15,6 +17,33 @@ namespace OAH_Evaluation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a new connection
+            var arduino = new ArduinoUno("COM4");
+
+            // Read an analog value 
+            float valueInVolts = arduino.ReadAnalog(ArduinoUnoAnalogPins.A0);
+
+            // Read the state of a pin
+            arduino.GetCurrentPinState(ArduinoUnoPins.D8);
+
+            // Write a digital value to an output pin
+            arduino.SetPinMode(ArduinoUnoPins.D4, PinModes.Output);
+            arduino.SetDO(ArduinoUnoPins.D4, true);
+
+            // Write an analog value (PWM) to a PWM pin
+            arduino.SetPinMode(ArduinoUnoPins.D3_PWM, PinModes.PWM);
+            arduino.SetPWM(ArduinoUnoPWMPins.D3_PWM, 90);
+
+            // Use a servo
+            arduino.SetPinMode(ArduinoUnoPins.D9_PWM, PinModes.Servo);
+            arduino.SetServo(ArduinoUnoPins.D9_PWM, 90);
+
+            // dispose of the object
+            arduino.Dispose();
         }
     }
 }
