ffc871337c0ecc1123607ed754bf24737ef373b0
qurihara <qurihara@gmail.com>
2015-07-15 09:50:47 +0900

    servo ok

--- OAH_Evaluation/Form1.cs
+++ OAH_Evaluation/Form1.cs
@@ -10,40 +10,74 @@ using System.Windows.Forms;
 using Sharpduino;
 using Sharpduino.Constants;
 
+            // Read an analog value 
+            //float valueInVolts = arduino.ReadAnalog(ArduinoUnoAnalogPins.A0);
+
+            // Read the state of a pin
+            //arduino.GetCurrentPinState(ArduinoUnoPins.D8);
+
+            // Write a digital value to an output pin
+            //arduino.SetPinMode(ArduinoUnoPins.D13, PinModes.Output);
+            //arduino.SetDO(ArduinoUnoPins.D13, true);
+
+            //// Write an analog value (PWM) to a PWM pin
+            //arduino.SetPinMode(ArduinoUnoPins.D3_PWM, PinModes.PWM);
+            //arduino.SetPWM(ArduinoUnoPWMPins.D3_PWM, 90);
+
+            //// Use a servo
+            //arduino.SetPinMode(ArduinoUnoPins.D9_PWM, PinModes.Servo);
+            //arduino.SetServo(ArduinoUnoPins.D9_PWM, 90);
+
+
 namespace OAH_Evaluation
 {
     public partial class Form1 : Form
     {
+        ArduinoUno arduino;
         public Form1()
         {
             InitializeComponent();
+
+            // Create a new connection
+            //var arduino = new ArduinoUno("COM3");
+            arduino = new ArduinoUno("COM3");
+            arduino.SetPinMode(ArduinoUnoPins.D13, PinModes.Output);
+
+            this.FormClosed += Form1_FormClosed;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            // Create a new connection
-            var arduino = new ArduinoUno("COM4");
-
-            // Read an analog value 
-            float valueInVolts = arduino.ReadAnalog(ArduinoUnoAnalogPins.A0);
-
-            // Read the state of a pin
-            arduino.GetCurrentPinState(ArduinoUnoPins.D8);
-
-            // Write a digital value to an output pin
-            arduino.SetPinMode(ArduinoUnoPins.D4, PinModes.Output);
-            arduino.SetDO(ArduinoUnoPins.D4, true);
-
-            // Write an analog value (PWM) to a PWM pin
-            arduino.SetPinMode(ArduinoUnoPins.D3_PWM, PinModes.PWM);
-            arduino.SetPWM(ArduinoUnoPWMPins.D3_PWM, 90);
-
-            // Use a servo
-            arduino.SetPinMode(ArduinoUnoPins.D9_PWM, PinModes.Servo);
-            arduino.SetServo(ArduinoUnoPins.D9_PWM, 90);
-
             // dispose of the object
             arduino.Dispose();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arduino.SetDO(ArduinoUnoPins.D13, true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            arduino.SetDO(ArduinoUnoPins.D13, false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            arduino.SetPinMode(ArduinoUnoPins.D9_PWM, PinModes.Servo);
+            arduino.SetPinMode(ArduinoUnoPins.D10_PWM, PinModes.Servo);
+            arduino.SetServo(ArduinoUnoPins.D9_PWM, 180);
+            arduino.SetServo(ArduinoUnoPins.D10_PWM,180);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            arduino.SetPinMode(ArduinoUnoPins.D9_PWM, PinModes.Servo);
+            arduino.SetPinMode(ArduinoUnoPins.D10_PWM, PinModes.Servo);
+            arduino.SetServo(ArduinoUnoPins.D9_PWM, 0);
+            arduino.SetServo(ArduinoUnoPins.D10_PWM, 0);
+
         }
     }
 }
